f1de17a235a26ec5fe2622f4c3e5889fa25bfc35
Rafa-W12 <rpintobiehl@gmail.com>
2026-04-24 10:00:46 -0300

    feat: Adição de um LED - Semáforo

--- blink.ino
+++ blink.ino
@@ -1,13 +1,19 @@
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);//Vermelho
+  pinMode(8, OUTPUT);//Amarelo
+  pinMode(7, OUTPUT);//Verde
+
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(8, LOW);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(7, HIGH);
   delay(1000);
   digitalWrite(8, HIGH);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  digitalWrite(7, LOW);
+  delay(500);
 }
+
